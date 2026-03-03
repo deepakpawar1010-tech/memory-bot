08945fd1c3297d214ce5e220761654911d6a74d9
deepakpawar1010-tech <deepak.pawar1010@gmail.com>
2026-03-03 20:09:35 +0530

    Adjusted manual threshold

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ APP_ID = os.getenv("APP_ID")
 APP_SECRET = os.getenv("APP_SECRET")
 
 DOC_THRESHOLD = 0.55
-MANUAL_THRESHOLD = 0.75
+MANUAL_THRESHOLD = 0.65
 
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
